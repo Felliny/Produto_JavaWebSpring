7a7b61ecee2c5401349ceabc4308db23740233e1
Luan <luancamilo313@gmail.com>
2024-04-15 09:36:31 -0300

    configuração do Spring

---
+++ src/main/java/br/edu/fateczl/Produto_JavaWebSpring/ProdutoJavaWebSpringApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.Produto_JavaWebSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoJavaWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoJavaWebSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fateczl/Produto_JavaWebSpring/ProdutoJavaWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.Produto_JavaWebSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoJavaWebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
